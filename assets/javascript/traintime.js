67ed2a24864daa63139e5290c0039ca0c4ff120f
Tierra Austin-Johnson <38711594+TbabyAJ@users.noreply.github.com>
2018-09-13 11:08:35 -0400

    adding js file and firebase

---
+++ assets/javascript/traintime.js
@@ -0,0 +1,91 @@
+// 1. Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDYxrnWiarxrYMY6hb7-WZVbJBqN6Mqs64",
+    authDomain: "traintime-1bba5.firebaseapp.com",
+    databaseURL: "https://traintime-1bba5.firebaseio.com",
+    projectId: "traintime-1bba5",
+    storageBucket: "",
+    messagingSenderId: "865825771391"
+  };
+  
+  firebase.initializeApp(config);
+  
+  var database = firebase.database();
+  
+  // 2. Button for adding Trains
+  $("#add-train-btn").on("click", function(event) {
+    event.preventDefault();
+  
+    // Grabs user input
+    var trainName = $("#train-name-input").val().trim();
+    var trainDestination = $("#destination-input").val().trim();
+    var empStart = moment($("#start-input").val().trim(), "MM/DD/YYYY").format("X");
+    var empRate = $("#rate-input").val().trim();
+  
+    // Creates local "temporary" object for holding train data
+    var newTrain = {
+      name: trainName,
+      destination: trainDestination,
+      start: empStart,
+      rate: empRate
+    };
+  
+    // Uploads train data to the database
+    database.ref().push(newTrain);
+  
+    // Logs everything to console
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.start);
+    console.log(newTrain.rate);
+  
+    alert("Train successfully added");
+  
+    // Clears all of the text-boxes
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#start-input").val("");
+    $("#rate-input").val("");
+  });
+  
+  // 3. Create Firebase event for adding train to the database and a row in the html when a user adds an entry
+  database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+  
+    // Store everything into a variable.
+    var trainName = childSnapshot.val().name;
+    var trainDestination = childSnapshot.val().role;
+    var empStart = childSnapshot.val().start;
+    var empRate = childSnapshot.val().rate;
+  
+    // Train Info
+    console.log(trainName);
+    console.log(trainDestination);
+    console.log(empStart);
+    console.log(empRate);
+  
+    // Prettify the train start
+    var empStartPretty = moment.unix(empStart).format("MM/DD/YYYY");
+  
+    // Calculate the months worked using hardcore math
+    // To calculate the months worked
+    var empMonths = moment().diff(moment(empStart, "X"), "months");
+    console.log(empMonths);
+  
+    // Calculate the total billed rate
+    var empBilled = empMonths * empRate;
+    console.log(empBilled);
+  
+    // Create the new row
+    var newRow = $("<tr>").append(
+      $("<td>").text(trainName),
+      $("<td>").text(trainDestination),
+      $("<td>").text(empStartPretty),
+      $("<td>").text(empMonths),
+      $("<td>").text(empRate),
+      $("<td>").text(empBilled)
+    );
+  
+    // Append the new row to the table
+    $("#train-table > tbody").append(newRow);
+  });
